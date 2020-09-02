8263ee05586d1aa5f36068834c27a9fbe96c1323
BankSinatra <51966182+BankSinatra@users.noreply.github.com>
2020-09-01 22:27:09 -0600

    I forgot to set ids for the buttons so I did

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
